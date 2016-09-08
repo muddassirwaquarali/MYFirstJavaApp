45172630564ebb4d31c3169f13ab2e45918260f8
muddassirwaquarali <muddassir.waquar@gmail.com>
2016-09-08 12:04:35 +0100

    added one line to print some text

--- src/myfirstjavaapp/MYFirstJavaApp.java
+++ src/myfirstjavaapp/MYFirstJavaApp.java
@@ -18,6 +18,7 @@ public class MYFirstJavaApp {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("This is my first Java App");
+        System.out.println("Still missing Build.xml");
         
     }
     
